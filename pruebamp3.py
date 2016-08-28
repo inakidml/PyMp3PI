f7115fca4dae9d2a35a9f3cc819f908b52d6c369
inakidml <inaki.diaz@ikasle.egibide.org>
2016-08-28 12:15:03 +0200

    sound -> wav

--- pruebamp3.py
+++ pruebamp3.py
@@ -6,7 +6,9 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400), 0, 32)
 pygame.mixer.init()
 pygame.mixer.music.load("2.mp3")
-
+print "music loaded"
+sound = pygame.mixer.Sound("2.wav")
+print "sound loaded"
 def texts(texto):
    font=pygame.font.SysFont(None ,32)
    text=font.render(texto, True,(255,255,255))
@@ -18,7 +20,7 @@ def texts(texto):
    pygame.display.flip()
 #texts("en marcha")
 pygame.mixer.music.play()
-
+print "playing music"
 loop = True
 
 while loop: #pygame.mixer.music.get_busy() == True:
@@ -28,13 +30,17 @@ while loop: #pygame.mixer.music.get_busy() == True:
 #	print event	
 	if event.type == KEYDOWN:
 		if event.key == K_p:
-	    	    texts("playing")
+	    	    texts("playing music")
 		    pygame.mixer.music.play()
+		if event.key == K_o:
+	    	    texts("playing sound")
+		    sound.play()
 		if event.key == K_s:
 		    texts("stop")
+		    sound.stop()
 		    pygame.mixer.music.stop()
 		    pygame.mixer.init()
 		    pygame.mixer.music.load("2.mp3")
 		if event.key == K_q:
 		    loop = False
-#    time.sleep(0.005)
+    time.sleep(0.005)
